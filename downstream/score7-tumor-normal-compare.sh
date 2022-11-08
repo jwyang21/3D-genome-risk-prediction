6c35e90e7acaae67ca35ed141f5c288f5962e89d
Jeewon Yang <jwyang.ai@gmail.com>
2022-11-08 12:15:50 +0900

    Add files via upload

---
+++ downstream/score7-tumor-normal-compare.sh
@@ -0,0 +1,5 @@
+for cohort in TCGA-BLCA TCGA-LUAD TCGA-THYM TCGA-PRAD TCGA-GBM TCGA-READ TCGA-KIRC TCGA-ESCA TCGA-STAD TCGA-UCEC TCGA-KIRP TCGA-SARC TCGA-THCA TCGA-HNSC TCGA-LIHC TCGA-LUSC TCGA-PCPG TCGA-SKCM TCGA-CESC TCGA-CHOL TCGA-PAAD TCGA-BRCA TCGA-COAD
+do
+	echo "python3 score7-tumor-normal-compare.py --cohort $cohort"
+	python3 score7-tumor-normal-compare.py --cohort $cohort
+done
