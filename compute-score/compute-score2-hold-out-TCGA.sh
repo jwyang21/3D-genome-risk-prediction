2b9bc4a7f4332927227868ef0251e5ea6a2c62f0
Jeewon Yang <jwyang.ai@gmail.com>
2022-11-21 19:21:36 +0900

    Update compute-score2-hold-out-TCGA.sh

--- compute-score/compute-score2-hold-out-TCGA.sh
+++ compute-score/compute-score2-hold-out-TCGA.sh
@@ -2,7 +2,7 @@ for cohort in TCGA-BLCA TCGA-LUAD TCGA-PRAD TCGA-KIRC TCGA-ESCA TCGA-UCEC TCGA-K
 do
 	for score_type in avg_pc1 pc1_fluctuation
 	do
-		echo "compute-score2-hold-out.py --cohort $cohort --reference_type TCGA --score_type $score_type"
-		python3 compute-score2-hold-out.py --cohort $cohort --reference_type TCGA --score_type $score_type
+		echo "compute-score2-score4-hold-out.py --cohort $cohort --reference_type TCGA --score_type $score_type"
+		python3 compute-score2-score4-hold-out.py --cohort $cohort --reference_type TCGA --score_type $score_type
 	done	
 done
