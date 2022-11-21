781a1bbfd5ae447d8e82a8ce835dcc139f319c93
Jeewon Yang <jwyang.ai@gmail.com>
2022-11-21 19:21:12 +0900

    Delete compute-score2-hold-out-PCBC.sh

--- compute-score/compute-score2-hold-out-PCBC.sh
+++
@@ -1,5 +0,0 @@
-for score_type in avg_pc1 pc1_fluctuation
-do
-	echo "compute-score2-hold-out.py --cohort PCBC --reference_type PCBC --score_type $score_type"
-	python3 compute-score2-hold-out.py --cohort PCBC --reference_type PCBC --score_type $score_type	
-done
