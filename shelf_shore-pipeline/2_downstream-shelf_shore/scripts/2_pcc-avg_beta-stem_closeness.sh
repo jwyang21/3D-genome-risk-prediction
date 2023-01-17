07ff9296fcd6e37efbd0155dfc0f1c0f37b1702f
Jeewon Yang <jwyang.ai@gmail.com>
2023-01-17 20:03:20 +0900

    Add files via upload

---
+++ shelf_shore-pipeline/2_downstream-shelf_shore/scripts/2_pcc-avg_beta-stem_closeness.sh
@@ -0,0 +1,12 @@
+#command example: bash 2_pcc-avg_beta-stem_closeness.sh > ../log/2_pcc-avg_beta-stem_closeness-ALL.log
+cpg_type=shelf_shore
+w_dir=/data/project/3dith/pipelines/$cpg_type-pipeline/2_downstream-$cpg_type
+score_f=/data/project/3dith/data/cohort-1-best-score-km.csv
+
+for cohort in TCGA-BLCA TCGA-LUAD TCGA-PRAD TCGA-KIRC TCGA-UCEC TCGA-KIRP TCGA-THCA TCGA-LIHC TCGA-LUSC TCGA-CHOL TCGA-PAAD TCGA-BRCA TCGA-COAD TCGA-ESCA TCGA-HNSC
+
+do
+	echo "python3 2_pcc-avg_beta-stem_closeness.py -w_dir $w_dir --cohort $cohort --cpg_type $cpg_type --score_fname $score_f"
+	python3 2_pcc-avg_beta-stem_closeness.py -w_dir $w_dir --cohort $cohort --cpg_type $cpg_type --score_fname $score_f
+done
+
