7d566c482211c23fe07d7c972f5e401d8e355874
Jeewon Yang <jwyang.ai@gmail.com>
2023-01-17 19:43:57 +0900

    Delete compute-reference-v2 directory

--- compute-reference-v2/compute-reference-vectors-PCBC.sh
+++
@@ -1,5 +0,0 @@
-for score_type in avg_pc1 pc1_fluctuation
-do
-	echo "python3 compute-reference-vectors.py --cohort PCBC --reference_type PCBC --score_type $score_type"
-	python3 compute-reference-vectors.py --cohort PCBC --reference_type PCBC --score_type $score_type
-done
