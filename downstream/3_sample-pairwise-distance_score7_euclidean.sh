525347383bf83554ddc260343d53e9fd0d10db06
Jeewon Yang <jwyang.ai@gmail.com>
2023-01-17 19:44:18 +0900

    Delete downstream directory

--- downstream/3_sample-pairwise-distance_score7_euclidean.sh
+++
@@ -1,5 +0,0 @@
-for cohort in TCGA-BLCA TCGA-CESC TCGA-COAD TCGA-GBM TCGA-KIRC TCGA-LIHC TCGA-LUSC TCGA-PCPG TCGA-READ TCGA-SKCM TCGA-THCA TCGA-UCEC TCGA-BRCA TCGA-CHOL TCGA-ESCA TCGA-HNSC TCGA-KIRP TCGA-LUAD TCGA-PAAD TCGA-PRAD TCGA-SARC TCGA-STAD TCGA-THYM
-do
-	echo "python3 3_sample-pairwise-distance_score7.py --score score7 --cohort $cohort"
-	python3 3_sample-pairwise-distance_score7.py --score score7 --cohort $cohort
-done
