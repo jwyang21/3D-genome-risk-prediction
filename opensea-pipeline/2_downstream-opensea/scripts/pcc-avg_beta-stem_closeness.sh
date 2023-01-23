b2a54ee8180737d6ce1ac4cbb1c67fc622760365
Jeewon Yang <jwyang.ai@gmail.com>
2023-01-23 16:05:42 +0900

    Update pcc-avg_beta-stem_closeness.sh

--- opensea-pipeline/2_downstream-opensea/scripts/pcc-avg_beta-stem_closeness.sh
+++ opensea-pipeline/2_downstream-opensea/scripts/pcc-avg_beta-stem_closeness.sh
@@ -1,4 +1,3 @@
-#command: bash 2_pcc-avg_beta-stem_closeness.sh > ../log/2_pcc-avg_beta-stem_closeness-ALL.log
 cpg_type=opensea
 w_dir=/data/project/3dith/pipelines/$cpg_type-pipeline/2_downstream-$cpg_type
 score_f=/data/project/3dith/data/cohort-1-best-score-km.csv
@@ -9,4 +8,3 @@ do
 	echo "python3 pcc-avg_beta-stem_closeness.py -w_dir $w_dir --cohort $cohort --cpg_type $cpg_type --score_fname $score_f"
 	python3 pcc-avg_beta-stem_closeness.py -w_dir $w_dir --cohort $cohort --cpg_type $cpg_type --score_fname $score_f
 done
-
