1dbe25a5a96746dc746cde65f27b8770ee530777
Jeewon Yang <jwyang.ai@gmail.com>
2023-01-23 15:57:56 +0900

    Update and rename 3_parse-avg_meth-score-corr-log.sh to parse-avg_meth-score-corr-log.sh

---
+++ opensea-pipeline/2_downstream-opensea/scripts/parse-avg_meth-score-corr-log.sh
@@ -0,0 +1,4 @@
+cpg_type=opensea
+
+echo "python3 parse-avg_meth-score-corr-log.py --cpg_type $cpg_type  > ../log/parse-avg_meth-score-corr-log.log"
+python3 parse-avg_meth-score-corr-log.py --cpg_type $cpg_type  > ../log/parse-avg_meth-score-corr-log.log
