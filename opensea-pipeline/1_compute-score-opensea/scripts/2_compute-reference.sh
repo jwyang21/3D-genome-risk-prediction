e0062bdd387015a03328355a4bffb19d2d1014ea
Jeewon Yang <jwyang.ai@gmail.com>
2023-01-23 13:22:27 +0900

    Update 2_compute-reference.sh

--- opensea-pipeline/1_compute-score-opensea/scripts/2_compute-reference.sh
+++ opensea-pipeline/1_compute-score-opensea/scripts/2_compute-reference.sh
@@ -6,9 +6,8 @@ do
     do
         for standardize in Y N
         do
-            echo "python3 $cpg_type-pipeline/1_compute-score-$cpg_type/scripts/2_compute-reference.py --standardize $standardize --cohort $cohort -w_dir $cpg_type-pipeline/1_compute-score-$cpg_type --pc1_upper_dir $cpg_type-pipeline/1_compute-score-$cpg_type/result -r_dir $cpg_type-pipeline/1_compute-score-$cpg_type/result --score_type $score_type --usage_option half -m_type $m_type"
-            python3 $cpg_type-pipeline/1_compute-score-$cpg_type/scripts/2_compute-reference.py --standardize $standardize --cohort $cohort -w_dir $cpg_type-pipeline/1_compute-score-$cpg_type --pc1_upper_dir $cpg_type-pipeline/1_compute-score-$cpg_type/result -r_dir $cpg_type-pipeline/1_compute-score-$cpg_type/result --score_type $score_type --usage_option half -m_type $m_type
+            echo "python3 2_compute-reference.py --standardize $standardize --cohort $cohort -w_dir /data/project/3dith/pipelines/$cpg_type-pipeline/1_compute-score-$cpg_type --pc1_upper_dir /data/project/3dith/pipelines/$cpg_type-pipeline/1_compute-score-$cpg_type/result -r_dir /data/project/3dith/pipelines/$cpg_type-pipeline/1_compute-score-$cpg_type/result --score_type $score_type --usage_option half -m_type $m_type"
+            python3 2_compute-reference.py --standardize $standardize --cohort $cohort -w_dir /data/project/3dith/pipelines/$cpg_type-pipeline/1_compute-score-$cpg_type --pc1_upper_dir /data/project/3dith/pipelines/$cpg_type-pipeline/1_compute-score-$cpg_type/result -r_dir /data/project/3dith/pipelines/$cpg_type-pipeline/1_compute-score-$cpg_type/result --score_type $score_type --usage_option half -m_type $m_type
         done
     done
 done
-~
