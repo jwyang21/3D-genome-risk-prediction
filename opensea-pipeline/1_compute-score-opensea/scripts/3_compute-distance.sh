25fed69dd01ba65659f82ed51f3c855b36dd4744
Jeewon Yang <jwyang.ai@gmail.com>
2023-01-23 14:41:23 +0900

    Update 3_compute-distance.sh

--- opensea-pipeline/1_compute-score-opensea/scripts/3_compute-distance.sh
+++ opensea-pipeline/1_compute-score-opensea/scripts/3_compute-distance.sh
@@ -16,13 +16,13 @@ do
 		do
 			for distance in euclidean cosine-sim
 			do
-				for m_type in iebdm bdm
+				for m_type in bdm iebdm
 				do
 					for num in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22
-                    do
-                        echo "python3 3_compute-distance.py --num_chrom $num --standardize $standardize --cohort $cohort --usage_option $usage_option -w_dir $w_dir --pc1_upper_dir $pc1_upper_dir --result_dir $result_dir --reference_dir $reference_dir --hg19_chr_len $hg19_chr_len --bdm_bin_dir $bdm_bin_dir --score_type $score_type --use_weighted_avg $use_weighted_avg --distance $distance --matrix_type $m_type --cpg_type $cpg_type"
-                        python3 3_compute-distance.py --num_chrom $num --standardize $standardize --cohort $cohort --usage_option $usage_option -w_dir $w_dir --pc1_upper_dir $pc1_upper_dir --result_dir $result_dir --reference_dir $reference_dir --hg19_chr_len $hg19_chr_len --bdm_bin_dir $bdm_bin_dir --score_type $score_type --use_weighted_avg $use_weighted_avg --distance $distance --matrix_type $m_type --cpg_type $cpg_type
-                    done
+                    			do
+                        			echo "python3 3_compute-distance.py --num_chrom $num --standardize $standardize --cohort $cohort --usage_option $usage_option -w_dir $w_dir --pc1_upper_dir $pc1_upper_dir --result_dir $result_dir --reference_dir $reference_dir --hg19_chr_len $hg19_chr_len --bdm_bin_dir $bdm_bin_dir --score_type $score_type --use_weighted_avg $use_weighted_avg --distance $distance --matrix_type $m_type --cpg_type $cpg_type"
+                        			python3 3_compute-distance.py --num_chrom $num --standardize $standardize --cohort $cohort --usage_option $usage_option -w_dir $w_dir --pc1_upper_dir $pc1_upper_dir --result_dir $result_dir --reference_dir $reference_dir --hg19_chr_len $hg19_chr_len --bdm_bin_dir $bdm_bin_dir --score_type $score_type --use_weighted_avg $use_weighted_avg --distance $distance --matrix_type $m_type --cpg_type $cpg_type
+                    			done
 				done
 			done
 		done
