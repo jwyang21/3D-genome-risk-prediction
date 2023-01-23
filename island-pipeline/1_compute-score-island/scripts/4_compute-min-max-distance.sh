73b5ea69c61fd1a710c550c6c464d92910e4540b
Jeewon Yang <jwyang.ai@gmail.com>
2023-01-23 14:35:22 +0900

    Delete island-pipeline/1_compute-score-island directory

--- island-pipeline/1_compute-score-island/scripts/4_compute-min-max-distance.sh
+++
@@ -1,4 +0,0 @@
-cpg_type=island
-manifest=/data/project/3dith/data/manifest_normal7.csv
-echo "python3 4_compute-min-max-distance.py --manifest_fname $manifest -w_dir /data/project/3dith/pipelines/$cpg_type-pipeline/1_compute-score-$cpg_type --score_dir /data/project/3dith/pipelines/$cpg_type-pipeline/1_compute-score-$cpg_type/result/ --result_dir /data/project/3dith/pipelines/$cpg_type-pipeline/1_compute-score-$cpg_type/result/ --usage_option half"
-python3 4_compute-min-max-distance.py --manifest_fname $manifest -w_dir /data/project/3dith/pipelines/$cpg_type-pipeline/1_compute-score-$cpg_type --score_dir /data/project/3dith/pipelines/$cpg_type-pipeline/1_compute-score-$cpg_type/result/ --result_dir /data/project/3dith/pipelines/$cpg_type-pipeline/1_compute-score-$cpg_type/result/ --usage_option half
