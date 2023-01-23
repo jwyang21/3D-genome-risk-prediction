e5e000ae18b21814aecb6db8648058cf41a3d21d
Jeewon Yang <jwyang.ai@gmail.com>
2023-01-23 17:17:03 +0900

    Create download.sh

---
+++ data/roadmap_epigenomics/download.sh
@@ -0,0 +1,12 @@
+wget https://egg2.wustl.edu/roadmap/data/byFileType/chromhmmSegmentations/ChmmModels/core_K27ac/jointModel/final/E066_18_core_K27ac_mnemonics.bed.gz
+wget https://egg2.wustl.edu/roadmap/data/byFileType/chromhmmSegmentations/ChmmModels/core_K27ac/jointModel/final/E075_18_core_K27ac_mnemonics.bed.gz
+wget https://egg2.wustl.edu/roadmap/data/byFileType/chromhmmSegmentations/ChmmModels/core_K27ac/jointModel/final/E079_18_core_K27ac_mnemonics.bed.gz
+wget https://egg2.wustl.edu/roadmap/data/byFileType/chromhmmSegmentations/ChmmModels/core_K27ac/jointModel/final/E096_18_core_K27ac_mnemonics.bed.gz
+wget https://egg2.wustl.edu/roadmap/data/byFileType/chromhmmSegmentations/ChmmModels/core_K27ac/jointModel/final/E098_18_core_K27ac_mnemonics.bed.gz
+wget https://egg2.wustl.edu/roadmap/data/byFileType/chromhmmSegmentations/ChmmModels/core_K27ac/jointModel/final/E119_18_core_K27ac_mnemonics.bed.gz
+gzip -d E066_18_core_K27ac_mnemonics.bed.gz
+gzip -d E075_18_core_K27ac_mnemonics.bed.gz
+gzip -d E079_18_core_K27ac_mnemonics.bed.gz
+gzip -d E096_18_core_K27ac_mnemonics.bed.gz
+gzip -d E098_18_core_K27ac_mnemonics.bed.gz
+gzip -d E119_18_core_K27ac_mnemonics.bed.gz
