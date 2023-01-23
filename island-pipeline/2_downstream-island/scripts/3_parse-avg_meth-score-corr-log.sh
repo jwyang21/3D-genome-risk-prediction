958a247b29ba0f0b10b110e2327e467ec82ccaf1
Jeewon Yang <jwyang.ai@gmail.com>
2023-01-23 14:35:31 +0900

    Delete island-pipeline/2_downstream-island directory

--- island-pipeline/2_downstream-island/scripts/3_parse-avg_meth-score-corr-log.sh
+++
@@ -1,4 +0,0 @@
-cpg_type=island
-
-echo "python3 3_parse-avg_meth-score-corr-log.py --cpg_type $cpg_type  > ../log/3_parse-avg_meth-score-corr-log.log"
-python3 3_parse-avg_meth-score-corr-log.py --cpg_type $cpg_type  > ../log/3_parse-avg_meth-score-corr-log.log
