b0de9eaabd1e41067c62a7d90c160ad647860407
Jeewon Yang <jwyang.ai@gmail.com>
2023-01-23 13:20:30 +0900

    Update 1_find-bdm-bins.sh

--- utils/scripts/1_find-bdm-bins.sh
+++ utils/scripts/1_find-bdm-bins.sh
@@ -1,4 +1,4 @@
-for cpg_type in opensea island shelf shore shelf_shore
+cpg_type=opensea
 
 do
     echo "python3 1_find-bdm-bins.py -w_dir /data/project/3dith/pipelines/utils --tcga_bdm_dir /data/project/3dith/pipelines/binned-difference-matrix-v2-$cpg_type/result --pcbc_bdm_dir /data/project/3dith/pipelines/binned-difference-matrix-pcbc/result --hg19_chr_len /data/project/3dith/data/hg19.fa.sizes --binsize 1000000 --save_dir /data/project/3dith/data/bdm_bins --cpg_type $cpg_type"
