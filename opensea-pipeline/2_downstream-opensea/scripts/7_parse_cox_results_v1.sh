5077de8bcbf19b0eed73bd57c6f65c37b9957d82
Jeewon Yang <jwyang.ai@gmail.com>
2023-01-23 15:03:21 +0900

    Delete 7_parse_cox_results_v1.sh

--- opensea-pipeline/2_downstream-opensea/scripts/7_parse_cox_results_v1.sh
+++
@@ -1,8 +0,0 @@
-cpg_type=opensea
-log_dir=/data/project/3dith/pipelines/$cpg_type-pipeline/2_downstream-$cpg_type/log
-version=1
-log_fname=7_parse_cox_results-v$version.txt
-
-echo "python3 7_parse_cox_results.py --version $version --log_dir $log_dir --log_fname $log_fname"
-python3 7_parse_cox_results.py --version $version --log_dir $log_dir --log_fname $log_fname
-
