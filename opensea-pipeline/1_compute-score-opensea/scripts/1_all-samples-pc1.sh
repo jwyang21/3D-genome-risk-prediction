7e1bd0bb59c440fec1b892e5379cc66695a2a0f1
Jeewon Yang <jwyang.ai@gmail.com>
2023-01-23 12:49:41 +0900

    Update 1_all-samples-pc1.sh

--- opensea-pipeline/1_compute-score-opensea/scripts/1_all-samples-pc1.sh
+++ opensea-pipeline/1_compute-score-opensea/scripts/1_all-samples-pc1.sh
@@ -3,8 +3,8 @@ for cohort in TCGA-BLCA TCGA-LUAD TCGA-PRAD TCGA-KIRC TCGA-ESCA TCGA-UCEC TCGA-K
 do
     for m_type in iebdm bdm
     do
-        echo "python3 1_all-samples-pc1.py --cohort $cohort -w_dir /data/project/3dith/pipelines/$cpg_type-pipeline/1_compute-score-$cpg_type --tcga_bdm_dir /data/project/3dith/pipelines/binned-difference-matrix-v2-$cpg_type/result --pcbc_bdm_dir /data/project/3dith/pipelines/binned-difference-matrix-pcbc/result -r_dir /data/project/3dith/pipelines/$cpg_type-pipeline/1_compute-score-$cpg_type/result -m_type $m_type"
+        echo "python3 $cpg_type-pipeline/1_compute-score-$cpg_type/scripts/1_all-samples-pc1.py --cohort $cohort -w_dir $cpg_type-pipeline/1_compute-score-$cpg_type --tcga_bdm_dir binned-difference-matrix-v2-$cpg_type/result --pcbc_bdm_dir binned-difference-matrix-pcbc/result -r_dir $cpg_type-pipeline/1_compute-score-$cpg_type/result -m_type $m_type"
 
-        python3 1_all-samples-pc1.py --cohort $cohort -w_dir /data/project/3dith/pipelines/$cpg_type-pipeline/1_compute-score-$cpg_type --tcga_bdm_dir /data/project/3dith/pipelines/binned-difference-matrix-v2-$cpg_type/result --pcbc_bdm_dir /data/project/3dith/pipelines/binned-difference-matrix-pcbc/result -r_dir /data/project/3dith/pipelines/$cpg_type-pipeline/1_compute-score-$cpg_type/result -m_type $m_type
+        python3 $cpg_type-pipeline/1_compute-score-$cpg_type/scripts/1_all-samples-pc1.py --cohort $cohort -w_dir $cpg_type-pipeline/1_compute-score-$cpg_type --tcga_bdm_dir binned-difference-matrix-v2-$cpg_type/result --pcbc_bdm_dir binned-difference-matrix-pcbc/result -r_dir $cpg_type-pipeline/1_compute-score-$cpg_type/result -m_type $m_type
     done
 done
