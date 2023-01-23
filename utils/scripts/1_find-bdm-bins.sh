6c849aef4a0a59b9b2489a43729e952343f0e7aa
Jeewon Yang <jwyang.ai@gmail.com>
2023-01-23 13:20:03 +0900

    Update 1_find-bdm-bins.sh

--- utils/scripts/1_find-bdm-bins.sh
+++ utils/scripts/1_find-bdm-bins.sh
@@ -1,5 +1,7 @@
-cpg_type=opensea
+for cpg_type in opensea island shelf shore shelf_shore
 
-echo "python3 utils/scripts/1_find-bdm-bins.py -w_dir utils --tcga_bdm_dir binned-difference-matrix-v2-$cpg_type/result --pcbc_bdm_dir binned-difference-matrix-pcbc/result --hg19_chr_len ../data/hg19.fa.sizes --binsize 1000000 --save_dir data/bdm_bins --cpg_type $cpg_type"
+do
+    echo "python3 1_find-bdm-bins.py -w_dir /data/project/3dith/pipelines/utils --tcga_bdm_dir /data/project/3dith/pipelines/binned-difference-matrix-v2-$cpg_type/result --pcbc_bdm_dir /data/project/3dith/pipelines/binned-difference-matrix-pcbc/result --hg19_chr_len /data/project/3dith/data/hg19.fa.sizes --binsize 1000000 --save_dir /data/project/3dith/data/bdm_bins --cpg_type $cpg_type"
 
-python3 utils/scripts/1_find-bdm-bins.py -w_dir utils --tcga_bdm_dir binned-difference-matrix-v2-$cpg_type/result --pcbc_bdm_dir binned-difference-matrix-pcbc/result --hg19_chr_len ../data/hg19.fa.sizes --binsize 1000000 --save_dir data/bdm_bins --cpg_type $cpg_type
+    python3 1_find-bdm-bins.py -w_dir /data/project/3dith/pipelines/utils --tcga_bdm_dir /data/project/3dith/pipelines/binned-difference-matrix-v2-$cpg_type/result --pcbc_bdm_dir /data/project/3dith/pipelines/binned-difference-matrix-pcbc/result --hg19_chr_len /data/project/3dith/data/hg19.fa.sizes --binsize 1000000 --save_dir /data/project/3dith/data/bdm_bins --cpg_type $cpg_type
+done
