5750727e20dd898c862717645466d09551d55840
Jeewon Yang <jwyang.ai@gmail.com>
2023-01-24 16:10:07 +0900

    Update 5_bdm-iebdm-pc1-pcc.py

--- utils/scripts/5_bdm-iebdm-pc1-pcc.py
+++ utils/scripts/5_bdm-iebdm-pc1-pcc.py
@@ -1,9 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[5]:
-
-
 import numpy as np
 import pandas as pd
 import os
@@ -13,59 +7,20 @@ import matplotlib.pyplot as plt
 import matplotlib as mpl
 import argparse
 
-mpl.rcParams['figure.dpi'] = 300 #150
+mpl.rcParams['figure.dpi'] = 300 
 plt.rc('font', family='FreeSans', size=7)
 plt.rc('figure', figsize=(1.5, 1.5))
-
 plt.rc('xtick', labelsize=7)
 plt.rc('ytick', labelsize=7)
 
-
-# In[3]:
-
-# command example: python3 230118_bdm-iebdm-pc1.py --cpg_type opensea > ../log/230118_bdm-iebdm-pc1_opensea.log
 cohorts = 'TCGA-BLCA TCGA-LUAD TCGA-PRAD TCGA-KIRC TCGA-ESCA TCGA-UCEC TCGA-KIRP TCGA-THCA TCGA-HNSC TCGA-LIHC TCGA-LUSC TCGA-CHOL TCGA-PAAD TCGA-BRCA TCGA-COAD'.split(' ')
-
-
-# In[4]:
-
-
-#cpg_type = 'opensea'
-
-
-# In[8]:
-
-
 CHR_LIST = [f'chr{i}' for i in np.arange(1, 23)]
-
-
-# In[37]:
-
-
-'''
-result_dir = f'/data/project/3dith/pipelines/{cpg_type}-pipeline/1_compute-score-{cpg_type}/result/bdm-iebdm-pc1'
-if not os.path.exists(result_dir):
-    os.makedirs(result_dir)
-'''
-
-
-# In[32]:
-
-
-SAMPLE_NAME_FILE = '/data/project/jeewon/research/3D-ITH/data/samplename.npz'#item: {cohort}_samples
-
-
-# In[ ]:
-
+SAMPLE_NAME_FILE = '/data/project/jeewon/research/3D-ITH/data/samplename.npz'
 
 def parse_arguments():
     args = argparse.ArgumentParser()
     args.add_argument('--cpg_type', type = str, required = True, help = 'opensea, island, shelf_shore')
     return args.parse_args()
-
-
-# In[ ]:
-
 
 if __name__=='__main__':
     args = parse_arguments()
@@ -92,7 +47,6 @@ if __name__=='__main__':
                         all_iebdm_samples_flist.append(f)
                     else:
                         all_bdm_samples_flist.append(f)
-
 
         assert len(all_bdm_samples_flist) == len(all_iebdm_samples_flist)
         for i in range(len(all_bdm_samples_flist)):
@@ -126,14 +80,10 @@ if __name__=='__main__':
                 
     avg_pcc_df = pd.DataFrame(index = cohorts, columns = CHR_LIST)
     fig = plt.figure(figsize = (2*5, 2*3))
-    #for i in range(1):
     for i in range(len(cohorts)):
         c = cohorts[i]
         ax = fig.add_subplot(3, 5, i+1)
-        #fig = plt.figure(figsize = (3*6, 3*4))
-        #globals()[f'{cohort}'] = []
         for chrom in CHR_LIST:
-            #ax = fig.add_subplot(4, 6, i+1)
             df_name = f'/data/project/3dith/pipelines/{args.cpg_type}-pipeline/1_compute-score-{args.cpg_type}/result/bdm-iebdm-pc1/{c}/{chrom}_bdm-iebdm-pcc.csv'
             df = pd.read_csv(df_name, index_col = 0)
             if df.isna().sum().sum() > 0:
@@ -141,11 +91,7 @@ if __name__=='__main__':
                 print('---\ndropped sample:')
                 print(df.iloc[df.isna().values.flatten(),:].index.values)
                 print("---")
-            pcc = df.pcc.dropna().values.flatten()#drop insignificant pcc, whose pval > 5e-2
-            #if df.isna().sum().sum():
-            #    print("\n\n\n\n\n"+c+' '+chrom)
-            #pcc = [abs(x) for x in pcc]
-            #lobals()[f'{cohort}'][chrom] = pcc
+            pcc = df.pcc.dropna().values.flatten()
             avg_pcc_df.loc[c][chrom] = np.mean(pcc)
         ax.hist(avg_pcc_df.loc[c].values.flatten())
         ax.set_title(c)
@@ -154,5 +100,3 @@ if __name__=='__main__':
     print(os.path.join(result_dir, 'pcc-bdm-iebdm-ALL-cohorts.png'))
     avg_pcc_df.to_csv(os.path.join(result_dir, 'pcc-bdm-iebdm-ALL-cohorts.csv'))
     print(os.path.join(result_dir, 'pcc-bdm-iebdm-ALL-cohorts.csv'))
-
-
