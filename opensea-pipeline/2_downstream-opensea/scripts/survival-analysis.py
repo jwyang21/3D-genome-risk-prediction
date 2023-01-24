567588a9a6d1692a173edd70b49acc2246852384
Jeewon Yang <jwyang.ai@gmail.com>
2023-01-24 15:33:50 +0900

    Update survival-analysis.py

--- opensea-pipeline/2_downstream-opensea/scripts/survival-analysis.py
+++ opensea-pipeline/2_downstream-opensea/scripts/survival-analysis.py
@@ -13,10 +13,9 @@ import argparse
 import pickle
 from sklearn.preprocessing import MinMaxScaler
 
-# lifelines 참고: https://www.notion.so/dohlee/Lifelines-survival-analysis-e929aae590d94037a585b8b1f42bbc2e
-# statannot 참고: https://partrita.github.io/posts/statannot/
+# lifelines: https://www.notion.so/dohlee/Lifelines-survival-analysis-e929aae590d94037a585b8b1f42bbc2e
+# statannot: https://partrita.github.io/posts/statannot/
 
-# default figure setting
 mpl.rcParams['figure.dpi'] = 150
 plt.rc('font', family = 'FreeSans', size = 7)
 plt.rc('figure', figsize = (1.5, 1.5))
@@ -26,14 +25,10 @@ CLINICAL_FNAME = '/data/project/3dith/data/TCGA-CDR-SupplementalTableS1.csv'
 P_THRESHOLD = 5e-2
 SAMPLE_NAME_FILE = '/data/project/3dith/data/samplenames.npz'#item: {cohort}
 
-def parse_arguments(): #default usage: score=stem-closeness, cohort=each TCGA cohort, s_type=avg-pc1, use_option=part, normalize = N, minmax = Y, w_dir = your working directory.
+def parse_arguments(): 
     args = argparse.ArgumentParser()
     args.add_argument('--score', help = 'Type of score', type = str, required = True)
     args.add_argument('--cohort', help = 'TCGA cohort', type = str, required = True)
-    
-    # below are all arguments for calculating stem-closeness.
-    
-    ## score 관련 옵션들 (avg_beta 및 brca subtype으로 돌릴 땐 빼도 됨)
     args.add_argument('--score_type', help = 'which one you will use among PC1 fluctuation or averaged PC1 vector. avg_pc1 or pc1_fluctuation', default = 'avg_pc1', required = False)
     args.add_argument('--usage_option', help = 'use all samples or randomly picked samples. all or part', default = 'part', required = False) # all, part
     args.add_argument('--normalize', help = 'whether you wnat to normalize score2 and score4 or not', default = 'N', required = False)#Y or N
@@ -44,28 +39,21 @@ def parse_arguments(): #default usage: score=stem-closeness, cohort=each TCGA co
     args.add_argument('--use_weighted_avg', help = 'whether to use weighted average in computing distance. Y or N (simple_avg)', type = str, required = True)
     args.add_argument('--standardize', help = 'whether to standardize PC1 or not when calculating distance. Y/N', type = str, required = True)
     args.add_argument('--num_chrom', help = 'chr1 to which chromosome you will use.5, 10, 15, 22.', type = int, default = 22, required = False)
-    
     args.add_argument('-w_dir', '--working_dir', help = 'working_directory', type = str, required = True)
     args.add_argument('--result_dir', help = 'result directory to save files', type = str, required = True)
-
-    # try both Y and N
-    #default: /data/project/3dith/pipelines/opensea-pipeline/1_compute-score-opensea/result/#TCGA-BRCA/
-
-    #score fname example: stem-closeness_euclidean_bdm_pc1-fluctuation_simple-avg_part_normalized.csv
     return args.parse_args()
 
 def get_sample_list(cohort):
-    # sample list of input TCGA cohort
     samples = np.load(SAMPLE_NAME_FILE)[cohort]
     S = samples.tolist()
     if cohort=='PCBC':
         T = []
         N = []
-    else: #TCGA cohort
+    else: 
         T = []
         N = []
         for s in samples:
-            if int(s[13:15]) >= 1 and int(s[13:15]) <= 9: #tumor barcode: '01' ~ '09'
+            if int(s[13:15]) >= 1 and int(s[13:15]) <= 9: 
                 T.append(s)
             elif int(s[13:15]) >=10 and int(s[13:15]) <= 19:
                 N.append(s)
@@ -73,68 +61,44 @@ def get_sample_list(cohort):
                 pass
     return T, N, S
 
-def merge(clinical_cohort, score_df, T):
-    # function 'merge': merge score and clinical data    
+def merge(clinical_cohort, score_df, T):   
     clinical_cohort.index = clinical_cohort['bcr_patient_barcode'].values.flatten()
-    #clinical_cohort.drop(['bcr_patient_barcode'], axis = 1, inplace = True)
-
     print("score shape: {}".format(score_df.shape))
     score_df['barcode'] = [x[:12] for x in score_df.index.values]
     score_df['full_barcode'] = score_df.index.values
     score_df['sample_type'] = ['Tumor' if score_df.index.values[i] in T else 'Normal' for i in range(score_df.shape[0])]
     score_df2 = score_df.drop_duplicates(subset = ['barcode']).copy() 
-    # barcode가 중복될 경우 keep = 'first'. 나머진 drop. # 동일 샘플에서 여러번 데이터가 수집된 경우가 여기에 해당됨
-    
     score_df2.index = score_df2.barcode.values
     score_df2.drop(['barcode'], axis = 1, inplace=True)
-    
     intersection = np.intersect1d(clinical_cohort.index.values, score_df2.index.values)
     intersection_tumor_only = np.intersect1d(clinical_cohort.index.values, score_df2[score_df2['sample_type']=='Tumor'].index.values)
     score_df2.drop(['sample_type'], axis = 1, inplace=True)
-    
-    # clinical과 score dataframe (현재 코호트의 전체 샘플들) 에서 겹치는 애들만 남기기
-    clinical_cohort2 = clinical_cohort.loc[intersection].copy().sort_index() # 전체 코호트 샘플들과 겹치는 애들만 남김.
-    score_df3 = score_df2.loc[intersection].copy().sort_index() # clinical과 겹치는 애들만 남김
-    
+    clinical_cohort2 = clinical_cohort.loc[intersection].copy().sort_index() 
+    score_df3 = score_df2.loc[intersection].copy().sort_index() 
     merged = pd.merge(clinical_cohort2, score_df3, left_index=True, right_index=True)
-
-    # clinical 과 score dataframe 에서 겹치는 'tumor' 샘플들만 남기기
     clinical_cohort3 = clinical_cohort.loc[intersection_tumor_only].copy().sort_index()
     score_df4 = score_df2.loc[intersection_tumor_only].copy().sort_index()
     print("final clinical data shape (tumor only): {}".format(clinical_cohort3.shape))
     print("final score data shape (tumor only): {}".format(score_df4.shape))
-    
     merged_tumor = pd.merge(clinical_cohort3, score_df4, left_index=True, right_index=True)
-    
     if 'sample_type' in merged.columns:
         print('Error! type is still in merged.columns')
-    
     if 'sample_type' in merged_tumor.columns:
         print('Error! type is still in merged_tumor.columns')
-    
-    return merged_tumor #make sure that 'group' column does not exist in 'merged'
-
-
-# In[11]:
-
+    return merged_tumor 
 
 def save_pickle(data, directory, fname):
     print("fname: {}".format(os.path.join(directory, fname)))
     data.to_pickle(os.path.join(directory, fname))
 
-
 def survival_analysis(df, target, q, directory, fig_width, figname, cohort):
-    # function 'survival_analysis': plot survival analysis results and save resulting figure.
-    
-    #fig_width = 4
-    fig = plt.figure(figsize = (4 * fig_width, fig_width)) #각 subplot은 정사각형 모양.
-    
+    fig = plt.figure(figsize = (4 * fig_width, fig_width)) 
     valid_t = []
     valid_t_pvalue = []
     sig_t = []
     sig_t_pvalue = []
     
-    for i, t in enumerate(target): # Iterate for all targets. 
+    for i, t in enumerate(target): 
         d = df[df[f'{t}.time'].notnull() & df[f'{t}'].notnull()].copy()
         if d.shape[0] !=0:
             valid_t.append(t)
@@ -143,21 +107,18 @@ def survival_analysis(df, target, q, directory, fig_width, figname, cohort):
 
             print("target = {}, num_samples = {}".format(t, d.shape[0]))
 
-            groups = list(range(q)) # 총 q개만큼의 그룹이 있음.
+            groups = list(range(q)) 
 
             ax = fig.add_subplot(1, 4, i+1)
 
-            for group in groups:#group 0 (low), group 1(High)
-                # T: event가 일어나기까지의 시간.
-                # E: event의 발생 여부.
+            for group in groups:
                 T = d[d.group==group][f'{t}.time'].values
-                E = d[d.group==group][f'{t}'].astype(bool).values #0, 1을 False, True로 바꿈.
+                E = d[d.group==group][f'{t}'].astype(bool).values 
 
                 kmf = lifelines.KaplanMeierFitter()
                 kmf.fit(T, E, label = ['Low', 'High'][group])
 
-                kmf.plot_survival_function(ax = ax, ci_show = False, linewidth = 3, xticks = [0, 2000], yticks = [0.2, 0.4, 0.6, 0.8, 1.0]) #ci_show: show confidence intervals if True.
-                #kmf.plot_survival_function(ax = ax, ci_show = False) #ci: confidence interval
+                kmf.plot_survival_function(ax = ax, ci_show = False, linewidth = 3, xticks = [0, 2000], yticks = [0.2, 0.4, 0.6, 0.8, 1.0]) 
             ax.get_xaxis().set_visible(True)
             ax.grid(False)
             ax.set_facecolor('white')
@@ -166,7 +127,7 @@ def survival_analysis(df, target, q, directory, fig_width, figname, cohort):
             ax.spines['left'].set_color('black')
             ax.spines['right'].set_color('black')
 
-            res = lifelines.statistics.logrank_test( #input: T_group0, T_group1, E_group0, E_group1
+            res = lifelines.statistics.logrank_test( 
                 d[d.group==0][f'{t}.time'].values,
                 d[d.group==q-1][f'{t}.time'].values,
                 d[d.group==0][f'{t}'].values,
@@ -184,10 +145,9 @@ def survival_analysis(df, target, q, directory, fig_width, figname, cohort):
             if res.p_value < P_THRESHOLD:
                 sig_t.append(t)
                 sig_t_pvalue.append(res.p_value)
-            ax.set_title(f'{t}, p = {res.p_value:.2g}', fontsize = 13, pad = 5) #pad: The offset of the title from the top of the axes, in points. Default is None to use rcParams['axes.titlepad'].
+            ax.set_title(f'{t}, p = {res.p_value:.2g}', fontsize = 13, pad = 5) 
             ax.set_xlabel('Days', fontsize = 7)
             ax.set_ylabel('Survival Probability', fontsize = 7)
-            #ax.set_xlim([0, 10000])
     fig.suptitle('Survival analysis ('+cohort+')', fontsize = 15)
     fig.subplots_adjust(wspace = 0.3)
     fig.tight_layout()
@@ -209,11 +169,10 @@ if __name__=='__main__':
     
     clinical = pd.read_csv(CLINICAL_FNAME)
     score_fname = args.score+'_'+args.distance+'_'+args.matrix_type+'_'+args.score_type+'_'+weighted_avg_flag+'_'+args.usage_option + normalize_yn+minmax_yn+standardize_flag+num_chrom_flag+'.csv'
-    #score filename 예시: 'stem-closeness_'+args.distance+'_'+args.matrix_type+'_'+args.score_type+'_'+weighted_avg_flag+'_'+args.usage_option + normalize_yn+minmax_yn+'.csv')
-    survival_target = ['OS', 'DSS', 'DFI', 'PFI'] # survival analysis #these variables are included in 'clinical_cohort'
+    survival_target = ['OS', 'DSS', 'DFI', 'PFI']
     result_filenme_default = args.score+'_'+args.distance+'_'+args.matrix_type+'_'+args.score_type+'_'+weighted_avg_flag+'_'+args.usage_option + normalize_yn+minmax_yn+standardize_flag+num_chrom_flag
     
-    cohort_dir = os.path.join(args.result_dir, args.cohort, 'kaplan-meier') #결과 저장할 때 씀. 
+    cohort_dir = os.path.join(args.result_dir, args.cohort, 'kaplan-meier') 
 
     if not os.path.exists(args.result_dir):
         os.makedirs(args.result_dir)
@@ -225,7 +184,7 @@ if __name__=='__main__':
     print("===\ncohort: {}".format(args.cohort))
     
     cohort = args.cohort
-    clinical_cohort = clinical[clinical['type'] == cohort.split('-')[-1]].copy() # clinical data of current cohort
+    clinical_cohort = clinical[clinical['type'] == cohort.split('-')[-1]].copy() 
 
     T, N, S = get_sample_list(args.cohort)
     print("len(tumor), len(normal), len(all): {}, {}, {}, respectively.".format(len(T), len(N), len(S)))
@@ -244,8 +203,7 @@ if __name__=='__main__':
     print("merged_tumor fname: ", os.path.join(cohort_dir, merged_tumor_fname))
 
     valid_t, valid_t_pvals, sig_t, sig_t_pvals = survival_analysis(merged_tumor, survival_target, 2, cohort_dir, 3, 
-            'survival_analysis_'+result_filenme_default+'.png', args.cohort) 
-    #Tumor samples only #생존분석 변수 column 전체가 NaN인 경우도 있음. 
+            'survival_analysis_'+result_filenme_default+'.png', args.cohort)  
 
     print("survival analysis: {}".format(os.path.join(cohort_dir, 'survival_analysis_pvalues_'+result_filenme_default+'.npz')))
     np.savez(os.path.join(cohort_dir, 'survival_analysis_pvalues_'+result_filenme_default), 
