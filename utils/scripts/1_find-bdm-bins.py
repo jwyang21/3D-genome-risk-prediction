3b632d91dac3e0aadebb2d8910897713ae497472
Jeewon Yang <jwyang.ai@gmail.com>
2023-01-24 16:05:10 +0900

    Update 1_find-bdm-bins.py

--- utils/scripts/1_find-bdm-bins.py
+++ utils/scripts/1_find-bdm-bins.py
@@ -1,47 +1,28 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[ ]:
-
-
 import pandas as pd
 import numpy as np
 import os
 import argparse
 import random
 
-
-# In[ ]:
-
-
 random.seed(2022)
 np.random.seed(2022)
 
-
-# In[ ]:
-
-
 ALL_COHORT = 'TCGA-LGG TCGA-UCS TCGA-BLCA TCGA-LUAD TCGA-THYM TCGA-PRAD TCGA-DLBC TCGA-ACC TCGA-KICH TCGA-GBM TCGA-READ TCGA-KIRC TCGA-LAML TCGA-ESCA TCGA-STAD TCGA-UCEC TCGA-KIRP TCGA-OV TCGA-SARC TCGA-THCA TCGA-HNSC TCGA-LIHC TCGA-LUSC TCGA-PCPG TCGA-SKCM TCGA-TGCT TCGA-CESC TCGA-CHOL TCGA-PAAD TCGA-UVM TCGA-MESO TCGA-BRCA TCGA-COAD'.split(' ')
 CHR_LIST = ['chr'+str(i) for i in np.arange(1, 23)]
-SAMPLE_NAME_FILE = '/data/project/3dith/data/samplenames.npz'#item: {cohort}
+SAMPLE_NAME_FILE = '/data/project/3dith/data/samplenames.npz'
 NORMAL7_COHORT_PCBC = 'TCGA-BLCA TCGA-LUAD TCGA-PRAD TCGA-KIRC TCGA-ESCA TCGA-UCEC TCGA-KIRP TCGA-THCA TCGA-HNSC TCGA-LIHC TCGA-LUSC TCGA-CHOL TCGA-PAAD TCGA-BRCA TCGA-COAD PCBC'.split(' ')
 
-
-# In[ ]:
-
-
 def get_sample_list(cohort):
-    # sample list of input TCGA cohort
     samples = np.load(SAMPLE_NAME_FILE)[cohort]
     S = samples.tolist()
     if cohort=='PCBC':
         T = []
         N = [] 
-    else: #TCGA cohort
+    else: 
         T = []
         N = []
         for s in samples:
-            if int(s[13:15]) >= 1 and int(s[13:15]) <= 9: #tumor barcode: '01' ~ '09'
+            if int(s[13:15]) >= 1 and int(s[13:15]) <= 9: 
                 T.append(s)
             elif int(s[13:15]) >=10 and int(s[13:15]) <= 19:
                 N.append(s)
@@ -49,49 +30,24 @@ def get_sample_list(cohort):
                 pass
     return T, N, S
 
-
-# In[ ]:
-
-
 def parse_arguments():
     args = argparse.ArgumentParser()
     args.add_argument('-w_dir', '--working_dir', type = str, required = True)
-
-    args.add_argument('--cpg_type', help = 'opensea, island, shelf, shore, shelf_shore', type = str, required = True)
-    
+    args.add_argument('--cpg_type', help = 'opensea, island, shelf, shore, shelf_shore', type = str, required = True)    
     args.add_argument('--tcga_bdm_dir', help = 'TCGA binned difference matrix directory', type = str, required = True)
-    #default: '/data/project/3dith/pipelines/binned-difference-matrix-v2-{cpg_type}/result' #이 디렉토리 내의 cohort 디렉토리에 IEBDM들 있음.
-
-    args.add_argument('--pcbc_bdm_dir', help = 'PCBC binned difference matrix directory', type = str, required = True)
-    #default: '/data/project/3dith/pipelines/binned-difference-matrix-pcbc/result' #IEBDM들이 저장된 디렉토리
-    
-    args.add_argument('--hg19_chr_len', help = 'hg19 chromosome length file', type = str, required = True)
-    #default: /data/project/3dith/data/hg19.fa.sizes'
-    
+    args.add_argument('--pcbc_bdm_dir', help = 'PCBC binned difference matrix directory', type = str, required = True)    
+    args.add_argument('--hg19_chr_len', help = 'hg19 chromosome length file', type = str, required = True)    
     args.add_argument('-b', '--binsize', type = int, required = True, default = int(1e6))
-
-    args.add_argument('--save_dir', type = str, required = True)
-    #/data/project/3dith/data/bdm_bins
-    
+    args.add_argument('--save_dir', type = str, required = True)    
     return args.parse_args()
 
-
-# In[ ]:
-
-
-def import_bdm(directory, chrom, s): #directory should be data_dir
+def import_bdm(directory, chrom, s): 
     f = os.path.join(directory, s+'.npz')
     diffmat = np.load(f)[chrom]
     diffmat_mask = np.load(f)[chrom+'_mask']
     diffmat_masked = diffmat[~diffmat_mask].T[~diffmat_mask].T
-
     return diffmat_masked
 
-
-# In[ ]:
-
-
-# In[12]:
 if __name__=='__main__':
     
     args = parse_arguments()
@@ -114,7 +70,7 @@ if __name__=='__main__':
         if 'TCGA' in cohort:
             bdm_dir = args.tcga_bdm_dir
             data_dir = os.path.join(bdm_dir, cohort)
-        else:#PCBC
+        else:
             bdm_dir = args.pcbc_bdm_dir
             data_dir = bdm_dir
             
@@ -125,7 +81,7 @@ if __name__=='__main__':
             diffmat_mask = np.load(os.path.join(data_dir, S[0]+'.npz'))[chrom+'_mask']
             globals()[cohort+'_diffmat_bins'][chrom+'_bins'] = np.array(bin_index)[~diffmat_mask].tolist()
             
-        result_fname = cohort+'_diffmat_bins'#npz
+        result_fname = cohort+'_diffmat_bins'
         
         np.savez(os.path.join(args.save_dir, args.cpg_type, result_fname), **globals()[cohort+'_diffmat_bins'])
         
