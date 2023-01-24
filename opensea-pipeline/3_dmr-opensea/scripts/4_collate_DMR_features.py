59e98f270f7892785ec7124e5818a8d6780c3fa0
Jeewon Yang <jwyang.ai@gmail.com>
2023-01-24 15:46:51 +0900

    Update 4_collate_DMR_features.py

--- opensea-pipeline/3_dmr-opensea/scripts/4_collate_DMR_features.py
+++ opensea-pipeline/3_dmr-opensea/scripts/4_collate_DMR_features.py
@@ -1,13 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-'''
-- Parse DMR feature results (for all 22 autosomes)
-- Collect DMR features per cohort and per threshold, and write all of them into an output file. 
-- DMR feature fname: /data/project/3dith/pipelines/island-pipeline/3_dmr-island/result/{cohort}/DMR_{big_category}-ANNOT_{small_category}_threshold_{threshold}_binsize_{binsize}.pickle
-'''
-# In[1]:
-
-
 import os
 import pandas as pd
 import numpy as np
@@ -15,8 +5,6 @@ import pickle
 import sys
 import argparse
 
-
-# global variables
 NORMAL7_COHORT = 'TCGA-BLCA TCGA-LUAD TCGA-PRAD TCGA-KIRC TCGA-ESCA TCGA-UCEC TCGA-KIRP TCGA-THCA TCGA-HNSC TCGA-LIHC TCGA-LUSC TCGA-CHOL TCGA-PAAD TCGA-BRCA TCGA-COAD'.split(' ')
 BIG_CATEGORY = ['GENE', 'REG', 'EPI']
 THRESHOLD = ['mean', 'mean_std', 'mean_2std']
@@ -26,26 +14,19 @@ if os.path.exists(SMALL_CATEGORY_FNAME):
 else:
     SMALL_CATEGORY = {}
     SMALL_CATEGORY['GENE'] = ['gene','transcript']
-    SMALL_CATEGORY['REG'] = ['open_chromatin_region', 'TF_binding_site', 'CTCF_binding_site', 'enhancer', 'promoter',                             'promoter_flanking_region']
-    SMALL_CATEGORY['EPI'] = ['18_Quies', '13_Het', '17_ReprPCWk', '16_ReprPC', '14_TssBiv', '2_TssFlnk',     '12_ZNF/Rpts', '11_EnhWk', '1_TssA', '6_TxWk', '5_Tx', '9_EnhA1', '7_EnhG1',     '4_TssFlnkD' ,'15_EnhBiv', '10_EnhA2', '3_TssFlnkU', '8_EnhG2']
+    SMALL_CATEGORY['REG'] = ['open_chromatin_region', 'TF_binding_site', 'CTCF_binding_site', 'enhancer', 'promoter','promoter_flanking_region']
+    SMALL_CATEGORY['EPI'] = ['18_Quies', '13_Het', '17_ReprPCWk', '16_ReprPC', '14_TssBiv', '2_TssFlnk','12_ZNF/Rpts', '11_EnhWk', '1_TssA', '6_TxWk', '5_Tx', '9_EnhA1', '7_EnhG1',     '4_TssFlnkD' ,'15_EnhBiv', '10_EnhA2', '3_TssFlnkU', '8_EnhG2']
     np.savez(SMALL_CATEGORY_FNAME,**SMALL_CATEGORY)
-    
 cohort2eid = pd.read_csv('/data/project/3dith/data/etc/cohort2eid.txt', sep = '\t', header = None)
 cohort2eid.columns = ['cohort', 'eid']
 eid_cohorts = cohort2eid.cohort.values
-
-
-# In[16]:
-
 
 def parse_arguments():
     parser = argparse.ArgumentParser()
     parser.add_argument('-c', '--cohort', help = 'TCGA cohort', type = str, required = True)
     parser.add_argument('-b', '--binsize', help = 'binsize. default 1e6', type = int, default = int(1e6), required = False)
     parser.add_argument('-w_dir', '--working_dir', help = 'working directory', type = str, required = True)
-    #default w_dir: /data/project/3dith/pipelines/island-pipeline/3_dmr-island
     return parser.parse_args()
-
 
 if __name__=='__main__':
     args = parse_arguments()
@@ -64,17 +45,12 @@ if __name__=='__main__':
         if big_c=='EPI' and args.cohort not in eid_cohorts:
             print("{} has no matching EID.".format(args.cohort))
             continue
-        #print("---\nbig_category: ", big_c)
         small_category = SMALL_CATEGORY[big_c]
         for small_c in small_category:
             if '/' in small_c:
                 small_c = small_c.replace('/', '-')
-            #print("small_cateogry: ", small_c)
             for t in THRESHOLD:
-                if t == 'mean_std': # opensea 실험결과, mean_std일 때가 DMR에 있는 gene 개수가 제일 적절했으므로 이때만 run.
-                    #print(t)
-                    #print("threshold: ", t)
-                    # 읽어야 할 정보가 들어있는 파일
+                if t == 'mean_std': 
                     if big_c != 'EPI':
                         dmr_feature_fname = os.path.join(cohort_dir, 'DMR_'+big_c+'-ANNOT_'+small_c+'_threshold_'+t+'_binsize_'+str(args.binsize)+'.pickle')
                     else:
@@ -83,11 +59,7 @@ if __name__=='__main__':
                         with open(dmr_feature_fname, 'rb') as f:
                             dmr_feature = pickle.load(f)
                         f.close()
-                        #print("Parsing {}".format(dmr_feature_fname))
-
-                        # 읽어들인 정보를 쓸 파일
-                        dmr_feature_parse_fname = os.path.join(cohort_dir, 'DMR_'+big_c+'-ANNOT_'+small_c+'_threshold_'+t+'_binsize_'+str(args.binsize)+'_ALL')#.npy')
-
+                        dmr_feature_parse_fname = os.path.join(cohort_dir, 'DMR_'+big_c+'-ANNOT_'+small_c+'_threshold_'+t+'_binsize_'+str(args.binsize)+'_ALL')
                         dmr_feature_parse_all = []
                         cnt = 0
                         for k in list(dmr_feature.keys()):
@@ -102,5 +74,3 @@ if __name__=='__main__':
                             print(dmr_feature_parse_fname+'.npy')
                         else:
                             print("no DMR feature parsed for {}/{}/{}.".format(big_c, small_c, t))
-                        #print("Parsed data is saved in {}".format(dmr_feature_parse_fname))
-
