331a66ed4feaa95e1d6dc93cc28f29c41b9fa59f
Jeewon Yang <jwyang.ai@gmail.com>
2023-05-08 12:12:37 +0900

    Delete 7_gseapy-gene-functional-annot.sh

--- dmr-analysis/7_gseapy-gene-functional-annot.sh
+++
@@ -1,6 +0,0 @@
-cpg_type=opensea
-dmr_type=HL
-wd=/data/project/3dith/pipelines/$cpg_type-pipeline/4_$dmr_type-DMR-$cpg_type
-threshold=mean_std
-echo "python3 7_gseapy-gene-functional-annot.py -w_dir $wd --threshold $threshold --dmr_type $dmr_type"
-python3 7_gseapy-gene-functional-annot.py -w_dir $wd --threshold $threshold --dmr_type $dmr_type
