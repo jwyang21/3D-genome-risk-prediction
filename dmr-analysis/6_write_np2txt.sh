89582b71da3fbb7a74a522367366d30062bf1bd7
Jeewon Yang <jwyang.ai@gmail.com>
2023-05-08 12:12:30 +0900

    Delete 6_write_np2txt.sh

--- dmr-analysis/6_write_np2txt.sh
+++
@@ -1,7 +0,0 @@
-threshold=mean_std
-cpg_type=opensea
-dmr_type=HL
-wd=/data/project/3dith/pipelines/$cpg_type-pipeline/4_$dmr_type-DMR-$cpg_type
-
-echo "python3 6_write_np2txt.py --threshold $threshold -w_dir $wd --dmr_type $dmr_type"
-python3 6_write_np2txt.py --threshold $threshold -w_dir $wd --dmr_type $dmr_type
