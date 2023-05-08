e7fb1f39d54880ead502cb9bf16da893311c0fe3
Jeewon Yang <jwyang.ai@gmail.com>
2023-05-08 12:53:12 +0900

    Update 6_write_np2txt.py

--- dmr-analysis/6_write_np2txt.py
+++ dmr-analysis/6_write_np2txt.py
@@ -1,9 +1,6 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[2]:
-
-
 import numpy as np
 import pandas as pd
 import os
@@ -11,9 +8,6 @@ import sys
 import pickle
 import argparse
 
-
-# global variables
-#NORMAL7_COHORT = 'TCGA-BLCA TCGA-LUAD TCGA-PRAD TCGA-KIRC TCGA-ESCA TCGA-UCEC TCGA-KIRP TCGA-THCA TCGA-HNSC TCGA-LIHC TCGA-LUSC TCGA-CHOL TCGA-PAAD TCGA-BRCA TCGA-COAD'.split(' ')
 SCORE_COHORT = 'TCGA-BLCA TCGA-LUAD TCGA-PRAD TCGA-KIRC TCGA-UCEC TCGA-KIRP TCGA-THCA TCGA-LIHC TCGA-LUSC TCGA-CHOL TCGA-PAAD TCGA-BRCA TCGA-COAD'.split(' ')
 BIG_CATEGORY = ['GENE', 'REG', 'EPI']
 THRESHOLD = ['mean', 'mean_std', 'mean_2std']
@@ -31,42 +25,27 @@ cohort2eid.columns = ['cohort', 'eid']
 eid_cohorts = cohort2eid.cohort.values
 CHR_LIST = ['chr'+str(i) for i in np.arange(1, 23)]
 
-
-
 def parse_arguments():
     parser = argparse.ArgumentParser()
-    #args.add_argument('-c', '--cohort', help = 'TCGA cohort', type = str, required = True)
-    #args.add_argument('-b', '--binsize', help = 'binsize', type = int, required = True, default = int(1e6))
     parser.add_argument('-t', '--threshold', help = 'DMR threshold. mean, mean_std, mean_2std', type = str, required = True, default = 'mean_std')
     parser.add_argument('-w_dir', '--working_dir', help = 'working directory', type = str, required = True)
     parser.add_argument('--dmr_type', type = str, help = 'TN or HL', required = True)
     parser.add_argument('--event', help = 'survival event', type = str, required = True)
     parser.add_argument('--fold', help = 'fold number', type = str, required = True, default = 'fold1')
     parser.add_argument('--version', help = 'feature version', type = str, required = True, default = 'v7.1')
-    parser.add_argument('--lr', help = 'learning rate. 0.001, 0.0005, 0.0001', type = float, required = True, default = 0.001)
-    
+    parser.add_argument('--lr', help = 'learning rate. 0.001, 0.0005, 0.0001', type = float, required = True, default = 0.001)    
     parser.add_argument('-c', '--cohort', help = 'TCGA cohort', type = str, required = True)
     return parser.parse_args()
-
-
-# # GENE
-
-# ## DMR의 GENE feature ID들 (ENSG, ENST) 모아서 txt file로 쓰기. (cohort별, threshold별)
-
-# In[7]:
 
 if __name__=='__main__':
     args = parse_arguments()
     os.chdir(args.working_dir)
     
-    # SAVEDIR: 현재 cohort의 결과를 저장하는 디렉토리.
     if args.dmr_type != 'risk_HL':
         SAVEDIR = os.path.join(os.getcwd(), 'result', args.cohort)
     else:
         SAVEDIR = os.path.join(os.getcwd(), 'result', args.cohort, f'{args.version}_lr_{args.lr}_{args.event}_{args.fold}')
 
-    #for cohort in SCORE_COHORT: #cohort를 argument로 입력받는 것으로 변경. 
-        #cohort_dir = os.path.join(os.getcwd(), 'result', cohort)
     npz_fname = 'DMR_GENE_features_threshold_'+args.threshold+'_id_len.npz'
     full_npz_fname = os.path.join(SAVEDIR, npz_fname)
     current_ensg = np.load(full_npz_fname)['ENSG_all']
@@ -89,30 +68,3 @@ if __name__=='__main__':
             f.write("\n")
     f.close()
     print(result_enst_fname)
-
-
-    # # Regulatory features
-
-    # ## ENSR ID들을 모아서 txt file로 쓰기 (chort 별, threshold 별)
-
-    # In[8]:
-
-    '''
-    for cohort in SCORE_COHORT:
-        cohort_dir = os.path.join(os.getcwd(), 'result', cohort)
-        npz_fname = 'DMR_REG_features_threshold_'+args.threshold+'_id_len.npz'
-        full_npz_fname = os.path.join(cohort_dir, npz_fname)
-        current_ensr = np.load(full_npz_fname)['ENSR_all']
-        result_ensr_fname = os.path.join(cohort_dir, 'ENSR_'+args.threshold+'.txt')
-
-        f = open(result_ensr_fname, 'w')
-        for i in range(len(current_ensr)):
-            f.write(current_ensr[i])
-            if i != len(current_ensr)-1:
-                f.write("\n")
-        f.close()
-        print(result_ensr_fname)
-
-    '''
-
-
