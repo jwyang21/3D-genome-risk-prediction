274accef4d02534b01eb62839cd6415b65b99e5a
Jeewon Yang <jwyang.ai@gmail.com>
2023-05-08 12:11:51 +0900

    Delete 2_find_DMR.sh

--- dmr-analysis/2_find_DMR.sh
+++
@@ -1,6 +0,0 @@
-binsize=1000000
-cpg_type=opensea
-dmr_type=HL
-
-echo "python3 2_find_DMR.py --binsize $binsize -w_dir /data/project/3dith/pipelines/$cpg_type-pipeline/4_$dmr_type-DMR-$cpg_type --cpg_type $cpg_type --dmr_type $dmr_type"
-python3 2_find_DMR.py --binsize $binsize -w_dir /data/project/3dith/pipelines/$cpg_type-pipeline/4_$dmr_type-DMR-$cpg_type --cpg_type $cpg_type --dmr_type $dmr_type
